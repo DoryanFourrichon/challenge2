caa1bcdf21dc587028d41d66345716c29234de65
DoryanFourrichon <doryan@pilot-in.com>
2022-07-19 08:51:35 +0200

    créato

---
+++ wp-content/themes/themeChallenge2/pilopress/layouts/_cards_article/_cards_article.php
@@ -0,0 +1,96 @@
+<?php
+if (!function_exists('dd')) {
+    function dd($data)
+    {
+        ini_set("highlight.comment", "#969896; font-style: italic");
+        ini_set("highlight.default", "#FFFFFF");
+        ini_set("highlight.html", "#D16568");
+        ini_set("highlight.keyword", "#7FA3BC; font-weight: bold");
+        ini_set("highlight.string", "#F2C47E");
+        $output = highlight_string("<?php\n\n" . var_export($data, true), true);
+        echo "<div style=\"background-color: #1C1E21; padding: 1rem\">{$output}</div>";
+        die();
+    }
+}
+
+$args = array(
+    'post_type' => 'post'
+);
+
+$the_query = new WP_Query($args);
+
+
+// Section
+$configuration = pip_layout_configuration();
+$css_vars      = acf_maybe_get( $configuration, 'css_vars' );
+
+// Fields
+$section_intro = get_sub_field( 'section_intro' );
+$cards_article = get_sub_field('cards_article');
+// dd($cards_article);
+$section_end   = get_sub_field( 'section_end' );
+
+// Configuration
+$advanced_mode   = get_sub_field( 'advanced_mode' );
+$container_width = get_sub_field( 'container_width' );
+
+// Content width
+$content_width = pip_get_responsive_class( $container_width, $advanced_mode );
+?>
+<section <?php echo acf_maybe_get( $configuration, 'section_id' ); ?>
+    class="<?php echo acf_maybe_get( $configuration, 'section_class' ); ?>"
+    style="<?php echo apply_filters( 'pip/layout/section_style', '', $configuration ); ?>"
+    <?php echo apply_filters( 'pip/layout/section_attributes', '', $configuration ); ?>>
+
+    <?php // To add dynamic markup at the beginning of this layout
+    do_action( 'pip/layout/section_start', $configuration ); ?>
+
+    <div class="container">
+        <div class="mx-auto <?php echo $content_width; ?>">
+
+            <?php
+            // Intro
+            if ( $section_intro ) : ?>
+                <div class="section_intro <?php echo acf_maybe_get( $css_vars, 'section_intro' ); ?>">
+                    <?php echo $section_intro; ?>
+                </div>
+            <?php endif; ?>
+
+            <?php
+                if($the_query->have_posts()) {
+                    while( $the_query->have_posts()) {
+                        $the_query->the_post(); ?>
+                            <div class="cards">
+                                <?php if(has_post_thumbnail()) : ?>
+                                    <div class="thumbnail"><?php the_post_thumbnail() ?></div>
+                                    <div class="card__title"><?php the_title() ?></div>
+                                    <div class="card__content"><?php the_content() ?></div>
+                                <?php else : ?>
+                                    <div class="card__title"><?php the_title() ?></div>
+                                    <div class="card__content"><?php the_content() ?></div>
+                                <?php endif; ?>
+                            </div>
+                        <?php
+                    }
+                }
+                else{
+                  echo 'coucou';  
+                }
+            ?>
+
+            <?php 
+            // Outro
+            if ( $section_end ) : ?>
+                <div class="section_end <?php echo acf_maybe_get( $css_vars, 'section_end' ); ?>">
+                    <?php echo $section_end; ?>
+                </div>
+            <?php endif; ?>
+
+        </div>
+    </div>
+
+    <?php // To add dynamic markup at the end of this layout
+    do_action( 'pip/layout/section_end', $configuration ); ?>
+
+</section>
+<?php
